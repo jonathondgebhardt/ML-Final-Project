e78226885277546721fd9be7f03b56fe7a0e80ad
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2018-03-21 16:33:17 -0400

    Cleaned up script

--- preprocess.py
+++ preprocess.py
@@ -2,79 +2,78 @@
 
 import csv
 
-irnLookups = {}
-irnCount = 0
+
+def checkIntersection():
+    matched = []
+    count = 0
+    for row in districtInfo:
+        for irns in irnLookups.values():
+            if int(float(row[1])) in irns:
+                matched.append(int(float(row[1])))
+                break
+
+
+    for row in districtInfo:
+        if int(float(row[1])) not in matched:
+            print('Did not match', row[1], 'to irnLookups dictionary')
+
+
+    print('Matched', len(matched), 'out of', len(districtInfo), 'schools from districtInfo to irnLookups')
 
 
 # grad.csv is what helps us map IRN's to County names
 # Build IRN dictionary: (String)county name: (List)IRN
+irnLookups = {}
 with open('grad.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
         # Row 2 is district name
         # Row 0 is IRN
-        print(row[0])
         if row[2] not in irnLookups:
             if row[2] != 'County':
-                irnLookups[row[2]] = [row[0]]
+                irnLookups[row[2]] = [int(row[0])]
         else:
             temp = irnLookups.get(row[2])
-            temp.append(row[0])
+            temp.append(int(row[0]))
             irnLookups[row[2]] = temp
-         
-        irnCount += 1
 
 
-
-print('\nTotal schools in grad.csv:', irnCount)
-print('Total counties in grad.csv:', len(irnLookups))
-
-
-# TODO: Get all columns from this data set
 # mort.csv is all of the mortality data and is categorized by 
 # county name, which is why we had to relate IRN's to county
 # names.
 # Build mortality
-mortCounties = []
+mortality = []
 line = 0
 with open('mort.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
+        # Ohio lines are between 2081 and 2170
         if line > 2081 and line < 2170:
             temp = row[0].split(",")
-            mortCounties.append(temp[0].replace(' County', ''))
+            mortality.append(row)
+
+        # If we get to line 2170, we're done
+        if line >= 2170:
+            break
+
         line += 1
-
-print('Total counties in mort.csv:', len(mortCounties))
-
-mostSchools = ""
-count = 0
-for k in irnLookups:
-    if len(irnLookups[k]) > count:
-        count = len(irnLookups[k])
-        mostSchools = k
-
-print(mostSchools, 'has most schools at', count)
 
 
 # Begin parsing district.csv, which contains various information
 # on schools by district
 districtInfo = []
-line = 0
 with open('district.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
         districtInfo.append(row)
 
+
+# Trim off first two rows which are column names and empty space
 districtInfo = districtInfo[2:]
 
-count = 0
-for row in districtInfo:
-    for irns in irnLookups.values():
-        print(str(irns))
-        k = input()
-        if str(int(float(row[1]))) in irns:
-            count += 1
-            break
 
-print('Matched', count, 'schools from districtInfo to irnLookups')
+# Validate intersection between two data sets
+checkIntersection()
+
+
+
